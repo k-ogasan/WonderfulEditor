df09625d9a446b3362f37cf3756406e0946338e1
小川航生 <>
2025-08-24 10:50:24 +0900

    テストコードの修正を行った。

--- app/serializers/api/v1/article_preview_serializer.rb
+++ app/serializers/api/v1/article_preview_serializer.rb
@@ -2,4 +2,12 @@ class Api::V1::ArticlePreviewSerializer < ActiveModel::Serializer
   attributes :id, :title, :updated_at, :comments_count, :likes_count
 
   belongs_to :user, serializer: Api::V1::UserSerializer
+
+  def comments_count
+    object.comments.count
+  end
+
+  def likes_count
+    object.article_likes.count
+  end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -62,4 +62,9 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
   config.include FactoryBot::Syntax::Methods
+
+  # JSONレスポンスを取得するヘルパーメソッド
+  def json_response
+    JSON.parse(response.body)
+  end
 end
