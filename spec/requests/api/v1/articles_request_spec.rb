bf4a1b9ef3d07f11ded76b7ae96f3658b7533d95
小川航生 <>
2025-08-24 10:55:25 +0900

    まだ実装していないメソッドのテストをコメントアウトした。

--- spec/requests/api/v1/articles_request_spec.rb
+++ spec/requests/api/v1/articles_request_spec.rb
@@ -17,31 +17,32 @@ RSpec.describe "Api::V1::Articles", type: :request do
     end
   end
 
-  describe "GET /show" do
-    it "returns http success" do
-      get "/api/v1/articles/show"
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # TODO: 後で実装予定のアクション
+  # describe "GET /show" do
+  #   it "returns http success" do
+  #     get "/api/v1/articles/show"
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
-  describe "GET /create" do
-    it "returns http success" do
-      get "/api/v1/articles/create"
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe "GET /create" do
+  #   it "returns http success" do
+  #     get "/api/v1/articles/create"
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
-  describe "GET /update" do
-    it "returns http success" do
-      get "/api/v1/articles/update"
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe "GET /update" do
+  #   it "returns http success" do
+  #     get "/api/v1/articles/update"
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
-  describe "GET /destroy" do
-    it "returns http success" do
-      get "/api/v1/articles/destroy"
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe "GET /destroy" do
+  #   it "returns http success" do
+  #     get "/api/v1/articles/destroy"
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 end
